5a7c4ebe48e87af7a343b7a723a6a73919fcee99
Yeva Byzek <yeva@thenile.dev>
2022-08-29 13:06:14 -0400

    Email

--- data-plane/pulumi/src/reconciler.ts
+++ data-plane/pulumi/src/reconciler.ts
@@ -2,19 +2,19 @@ import Reconcile from "./commands/reconcile/index"
 
 import * as dotenv from "dotenv";
 
+dotenv.config()
+
+const basePath = process.env.NILE_URL || '';
+const workspace = process.env.NILE_WORKSPACE || '';
+const email = process.env.NILE_DEVELOPER_EMAIL || '';
+const password = process.env.NILE_DEVELOPER_PASSWORD || '';
+const organization = process.env.NILE_ORGANIZATION_ID || '';
+const entity = process.env.NILE_ENTITY_NAME || '';
+
+console.log ("email (env): ", process.env.NILE_DEVELOPER_EMAIL)
+console.log ("email (param): ", email)
+
 async function run() {
-
-  await dotenv.config()
-  console.log ("(TO REMOVE) test env: ", process.env)
-
-  const basePath = process.env.NILE_URL
-  const workspace = process.env.NILE_WORKSPACE
-  const email = process.env.NILE_DEVELOPER_EMAIL
-  const password = process.env.NILE_DEVELOPER_PASSWORD
-  const organization = process.env.NILE_ORGANIZATION_ID
-  const entity = process.env.NILE_ENTITY_NAME
-
-  console.log ("basePath: ", basePath)
 
   await Reconcile.run([
     '--basePath', basePath,
