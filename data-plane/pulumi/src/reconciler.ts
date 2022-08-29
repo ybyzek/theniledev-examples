56afee996f4dcad25c7ef00e47d5da1fa08398c8
Yeva Byzek <yeva@thenile.dev>
2022-08-29 13:21:00 -0400

    override if conflict

--- data-plane/pulumi/src/reconciler.ts
+++ data-plane/pulumi/src/reconciler.ts
@@ -2,7 +2,7 @@ import Reconcile from "./commands/reconcile/index"
 
 import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config({ override: true })
 
 const basePath = process.env.NILE_URL || '';
 const workspace = process.env.NILE_WORKSPACE_NAME || '';
@@ -10,16 +10,6 @@ const email = process.env.NILE_DEVELOPER_EMAIL || '';
 const password = process.env.NILE_DEVELOPER_PASSWORD || '';
 const organization = process.env.NILE_ORGANIZATION_ID || '';
 const entity = process.env.NILE_ENTITY_NAME || '';
-
-console.log ("email (env): ", process.env.NILE_DEVELOPER_EMAIL)
-
-console.log ("basePath: ", basePath)
-console.log ("workspace: ", workspace)
-console.log ("email: ", email)
-console.log ("password: ", password)
-console.log ("organization: ", organization)
-console.log ("entity: ", entity)
-
 
 async function run() {
 
