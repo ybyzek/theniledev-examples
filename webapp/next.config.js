e732b3465dea09e6fa5d054ef6cc53ad118798b3
Yeva Byzek <ybyzek@users.noreply.github.com>
2022-10-06 08:13:02 -0400

    fix(webapp): check for presence of .certificates folder for local certs (#73)

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -5,6 +5,13 @@ const fs = require('fs');
 const envPath = './.env';
 let runtimeConfig = {};
 
+// Validate user has taken the step to setup certificates
+const certificateDir = './.certificates';
+if (!fs.existsSync(certificateDir)) {
+  console.log(`Error: directory ${certificateDir} does not exist. Did you forget to setup your local certificates? Refer to the README.md for instructions`);
+  process.exit(1);
+}
+
 try {
   // reads from .env to expose values to the client
   const file = fs.readFileSync(envPath, 'utf8');
@@ -55,7 +62,7 @@ try {
     return accum;
   }, {});
 } catch (err) {
-  console.log('err: + ', err);
+  console.log('err: ', err);
   console.warn(
     '[ERROR] local .env file missing. This must be configured before the demo can be run. '
   );
